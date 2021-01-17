410db173580f58c4f66d92801901eae8e820b6b6
Durdona <durdona.jurayeva@mail.ru>
2021-01-17 20:44:02 +0500

    Admin Sign In

---
+++ app/src/main/java/com/example/justeatitadmin/Common/Common.kt
@@ -0,0 +1,8 @@
+package com.example.justeatitadmin.Common
+
+import com.example.justeatitadmin.Model.ServerUserModel
+
+object Common {
+    val SERVER_REF = "Server"
+    var currentServerUser: ServerUserModel?=null
+}
---
+++ app/src/main/java/com/example/justeatitadmin/Model/ServerUserModel.kt
@@ -0,0 +1,8 @@
+package com.example.justeatitadmin.Model
+
+class ServerUserModel {
+    var uid:String?=null
+    var name:String?=null
+    var phone:String?=null
+    var isActive:Boolean = false
+}
